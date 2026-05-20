104e438e207eaf75ba317a350c36ec6f04b78433
Dario Migliaccio <dariomigliaccio@Mac.lan>
2026-05-20 14:35:49 -0400

    chore(deploy): add cpanel node server

---
+++ server.js
@@ -0,0 +1,21 @@
+process.env.NODE_ENV = process.env.NODE_ENV || "production";
+
+/* eslint-disable @typescript-eslint/no-require-imports */
+const { createServer } = require("http");
+const next = require("next");
+/* eslint-enable @typescript-eslint/no-require-imports */
+
+const port = Number.parseInt(process.env.PORT || "3000", 10);
+const hostname = process.env.HOSTNAME || "127.0.0.1";
+const dev = process.env.NODE_ENV !== "production";
+
+const app = next({ dev, hostname, port });
+const handle = app.getRequestHandler();
+
+app.prepare().then(() => {
+  createServer((req, res) => {
+    handle(req, res);
+  }).listen(port, hostname, () => {
+    console.log(`Ready on http://${hostname}:${port}`);
+  });
+});
